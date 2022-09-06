58a7d4ce98fbb208144a57f4e833d6ccf8035a6d
AdrianoGalan <dricogalan@gmail.com>
2022-09-06 19:18:46 -0300

    inicio tempo entre brancas

---
+++ src/main/java/com/blaze/algoritmos/TempoEntreBrancas.java
@@ -0,0 +1,24 @@
+package com.blaze.algoritmos;
+
+import com.blaze.controllerview.PrincipalController;
+
+public class TempoEntreBrancas {
+	
+	private PrincipalController pc;
+
+	private int numeroSugestao = 0;
+	private int numeroAcerto = 0;
+	private int somaP = 0;
+	private int somaV = 0;
+	private String somaPV = "";
+
+	private boolean apostar = false;
+
+	public TempoEntreBrancas(PrincipalController pc) {
+		super();
+		this.pc = pc;
+	}
+	
+	
+
+}
